a78f51c95c50d2a54f8a1862e798e97d95f37e89
Pavan Kumar <ptpavankumar@hotmail.com>
2016-07-07 10:55:42 +1000

    Added basic React component

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>Learning React-Redux-Webpack</h1>
+        <p>Sample test blah blah blah</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
